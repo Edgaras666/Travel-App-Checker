59f7f0316cbb493aae5757694cccfff5cb667ffc
Edgaras Bertašius <bertasius.edgaras@gmail.com>
2026-04-21 00:18:03 +0300

    Allow Vercel frontend origin

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,8 @@ CORS(app, resources={
     r"/check": {
         "origins": [
             "http://localhost:3000",
-            "http://127.0.0.1:3000"
+            "http://127.0.0.1:3000",
+            "https://travel-app-checker.vercel.app/"
         ]
     }
 })
